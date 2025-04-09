502bb5aa7d3f07b7a75e389ebc56449b7ae67d81
Mugabepro-1 <mugabepromesse@gmail.com>
2025-04-09 21:58:29 +0200

    feat: Removed unwanted files and created official read me file

---
+++ README.md
@@ -0,0 +1,4 @@
+# ML Basics
+
+This repository contains my implementations and experiments
+to understand machine-learning fundamentals from first principles.
--- src/main/java/org/mupro/springbootapi/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.mupro.springbootapi.repository;
-
-import org.mupro.springbootapi.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-}
