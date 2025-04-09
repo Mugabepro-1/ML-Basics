3b4e94719da3f94eba9724a6924b4673351c1f90
Mugabepro-1 <mugabepromesse@gmail.com>
2025-04-09 22:30:22 +0200

    feat: mae calculation

--- utils/metrics.py
+++ utils/metrics.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def mean_squared_error(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2)
+
+def mean_absolute_error(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
