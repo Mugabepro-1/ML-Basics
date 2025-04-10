6dbcb466b11eabffa67f3d9cab69dbc71c446612
Mugabepro-1 <mugabepromesse@gmail.com>
2025-04-10 22:31:59 +0200

    feat: accuracy calculation

--- utils/metrics.py
+++ utils/metrics.py
@@ -10,3 +10,6 @@ def r2_score(y_true, y_pred):
     ss_res = np.sum((y_true - y_pred) ** 2)
     ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
     return 1 - ss_res / ss_tot
+
+def accuracy(y_true, y_pred):
+    return np.mean(y_true == y_pred)
