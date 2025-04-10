7eb56d05bddd9ff5b72dba16b5e241dc787f4406
Mugabepro-1 <mugabepromesse@gmail.com>
2025-04-10 22:35:35 +0200

    feat: f1_score calculation

--- utils/metrics.py
+++ utils/metrics.py
@@ -23,3 +23,8 @@ def recall(y_true, y_pred, pos_label=1):
     tp = np.sum((y_true == pos_label) & (y_pred == pos_label))
     fn = np.sum((y_true == pos_label) & (y_pred != pos_label))
     return tp / (tp + fn) if (tp + fn) > 0 else 0
+
+def f1_score(y_true, y_pred, pos_label=1):
+    p = precision(y_true, y_pred, pos_label)
+    r = recall(y_true, y_pred, pos_label)
+    return 2 * p * r / (p + r) if (p + r) > 0 else 0
